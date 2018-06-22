b9bcf7e8e037bb3e63186a47e152f3a9b335c758
starcon2 <starcon95@gmail.com>
2018-06-21 19:53:01 -0600

    Updated config.php
    Made a slightly better html safety function.

--- config.php
+++ config.php
@@ -7,8 +7,33 @@ $dbname = "h2";
 $conn = new mysqli($servername, $susername, $spassword, $dbname);
 $conn->set_charset("utf8");
 
-function htmlEncode($s) {
-    return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+function htmlEncode($input) {
+      $search = array(
+    '@<script[^>]*?>.*?</script>@si',   // Strip out javascript
+    '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
+    '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+    '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments
+  );
+
+    $output = preg_replace($search, '', $input);
+    return $output;
+  }
+?>
+<?php
+function sanitize($input) {
+    if (is_array($input)) {
+        foreach($input as $var=>$val) {
+            $output[$var] = sanitize($val);
+        }
+    }
+    else {
+        if (get_magic_quotes_gpc()) {
+            $input = stripslashes($input);
+        }
+        $input  = cleanInput($input);
+        $output = mysql_real_escape_string($input);
+    }
+    return $output;
 }
 
 if (session_status() == PHP_SESSION_NONE) {
